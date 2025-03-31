b7182af284473c48ff6a20c9549797cb9e7e4062
Quentin <quentin.garnierr@gmail.com>
2025-03-31 13:51:48 +0200

    final version

---
+++ Final_Version/main.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "🔄 Conversion des fichiers PDF en texte avec pdftotext..."
+
+DOSSIER_PDF="../CORPUS_TRAIN"
+DOSSIER_TEXTE="./corpus_txt"
+
+mkdir -p "$DOSSIER_TEXTE"
+
+# Vérifie si pdftotext est installé
+if ! command -v pdftotext &> /dev/null; then
+    echo "❌ Erreur : 'pdftotext' est introuvable. Installe-le avec : sudo apt install poppler-utils"
+    exit 1
+fi
+
+for fichier_pdf in "$DOSSIER_PDF"/*.pdf; do
+    nom_fichier=$(basename "$fichier_pdf" .pdf)
+    fichier_txt="$DOSSIER_TEXTE/$nom_fichier.txt"
+    fichier_temp="./temp_cleaned.txt"
+
+    echo "📄 Conversion de $fichier_pdf en $fichier_txt"
+    pdftotext -layout -enc UTF-8 -nopgbrk -eol unix "$fichier_pdf" "$fichier_txt"
+
+    if [ $? -ne 0 ]; then
+        echo "❌ Erreur lors de la conversion de $fichier_pdf"
+        continue
+    fi
+
+    echo "🧠 fichier $fichier_txt convertit"
+done
+
+echo "✅ Conversion et mise en page terminées pour tous les fichiers."
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Parseur-articles-scientifiques-en-format-texte
+## 📚 Parseur d'Articles Scientifiques en Texte
+
+Ce projet convertit automatiquement des articles scientifiques au format PDF en texte brut.
+
+---
+
+### 🧰 Prérequis
+
+il est necessaire d’avoir :
+
+- Un environnement **Linux/WSL** avec `bash`  
+- **Poppler-utils** (pour utiliser `pdftotext`)  
+  Installe-le si nécessaire :
+  ```bash
+  sudo apt update
+  sudo apt install poppler-utils
+  ```
+
+---
+
+### 📁 Structure
+
+```
+- CORPUS_TRAIN   # Dossier contenant les fichiers PDF à traiter
+- Final_Version
+  ├── main.sh               # Script principal de conversion + mise en forme
+  └── corpus_txt/             # Sortie texte générée automatiquement
+```
+
+> 📌 Les fichiers PDF doivent être placés dans `../CORPUS_TRAIN`  
+> Le script générera un `.txt` par PDF dans `./corpus_txt`
+
+---
+
+### 🚀 Lancer le script
+
+```bash
+chmod +x quentin.sh
+./main.sh
+```
